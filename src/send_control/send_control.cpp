fbf21adfc38f7d9ba7d4b5b4db9834e0c5f6fe55
FishSWA <fishswaa@gmail.com>
2025-03-27 22:15:10 +0800

    update

--- src/send_control/send_control.cpp
+++ src/send_control/send_control.cpp
@@ -57,7 +57,7 @@ void send_control(double yaw_set, double pitch_set, bool fire)
 {
     Vison_control pkg{};
 
-    pkg.header = 0xb6;
+    pkg.header = 0x6A;
     pkg.yaw_set = yaw_set;
     pkg.pitch_set = pitch_set;
     pkg.fire = fire;
