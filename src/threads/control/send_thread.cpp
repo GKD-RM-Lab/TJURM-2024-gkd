0e64118920d84b2f64b5cbe6efc8cbfb3637334c
FishSWA <fishswaa@gmail.com>
2025-03-28 11:37:56 +0800

    little debug

--- src/threads/control/send_thread.cpp
+++ src/threads/control/send_thread.cpp
@@ -296,7 +296,9 @@ void Control::send_thread() {
         
         if (get_frame.exchange(false)) {    //sync 
             {
-                send_control(socket_interface.pkg.yaw + target_yaw, -target_pitch, fire);
+                if(socket_interface.pkg.yaw + target_yaw < 0.001) {
+                    send_control(socket_interface.pkg.yaw + target_yaw, -target_pitch, fire);
+                }
                 timer2.end();
                 if(params.is_target_out){
                 std::cout << "target pitch \t" << -target_pitch << "\ttarget yaw \t" << socket_interface.pkg.yaw 
